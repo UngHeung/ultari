f8422dc14d508495b72335c0c6f26d4469d40a64
ungheung <t01047377477@gmail.com>
2024-08-25 00:12:58 +0900

    feat : jwt strategy 추가

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entity/user-entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {
+    super({
+      secretOrKey: 'secret',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: { userAccount: string }) {
+    const { userAccount } = payload;
+    const user: UserEntity = await this.userRepository.findOneBy({
+      userAccount,
+    });
+
+    if (!user) throw new UnauthorizedException();
+
+    return user;
+  }
+}
