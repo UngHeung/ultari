4e147d2070a2e4d3e1891770e3ca27e4c7fb53ed
ungheung <t01047377477@gmail.com>
2024-08-25 00:29:19 +0900

    feat : auth service 단 유저 repo 연결

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user-entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+}
