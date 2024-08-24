e0bf4f6e2e81c9a1becd2afa61ed56bda97e1493
ungheung <t01047377477@gmail.com>
2024-08-25 00:20:15 +0900

    refactor : config 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,16 @@ import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user-entity';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
 
+const jwtConfig: { secret: string; expiresIn: number } = config.get('jwt');
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'secret',
+      secret: jwtConfig.secret,
       signOptions: {
-        expiresIn: 60 * 60,
+        expiresIn: jwtConfig.expiresIn,
       },
     }),
     TypeOrmModule.forFeature([UserEntity]),
