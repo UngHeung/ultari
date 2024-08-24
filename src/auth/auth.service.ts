5a0f072d33c3212bbf63d06cf4e1e922999e00b4
ungheung <t01047377477@gmail.com>
2024-08-25 00:45:37 +0900

    feat : 유저 회원가입 기능

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user-entity';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
+import { UserSignupDto } from './dto/user-signup.dto';
 
 @Injectable()
 export class AuthService {
@@ -11,4 +16,17 @@ export class AuthService {
     private userRepository: Repository<UserEntity>,
     private jwtService: JwtService,
   ) {}
+
+  async createUser(userSignupDto: UserSignupDto): Promise<void> {
+    // userSignupDto.userPassword = 'bcypt'
+    const user = this.userRepository.create(userSignupDto);
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505')
+        throw new ConflictException('이미 등록된 계정입니다.');
+      else throw new InternalServerErrorException();
+    }
+  }
 }
