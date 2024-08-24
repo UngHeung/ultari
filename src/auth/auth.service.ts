3992335403ed4187e17301c17c4697d16b9fda5e
ungheung <t01047377477@gmail.com>
2024-08-25 00:30:11 +0900

    feat : auth service 단 jwt service 연결

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user-entity';
 import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
+    private jwtService: JwtService,
   ) {}
 }
