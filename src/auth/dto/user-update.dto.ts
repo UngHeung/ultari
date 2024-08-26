f4a736048b2bfa56b3af535486f64bedc097f7e7
ungheung <t01047377477@gmail.com>
2024-08-26 14:21:12 +0900

    refactor : id는 param으로 사용, id값 dto에서 제거

---
+++ src/auth/dto/user-update.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { UserProfileEntity } from '../entity/user-profile.entity';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  userPassword: string;
+
+  @IsString()
+  userPhone: string;
+  @IsString()
+  userEmail: string;
+
+  userProfile: UserProfileEntity;
+}
