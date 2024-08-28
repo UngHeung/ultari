3692760bc7d2dd672b3d122d2858290fcffd59e0
ungheung <t01047377477@gmail.com>
2024-08-29 01:19:11 +0900

    feat : id, createAt, updateAt 상속용 entity 추가

---
+++ src/common/entity/base.entity.ts
@@ -0,0 +1,12 @@
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+export class BaseModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+}
