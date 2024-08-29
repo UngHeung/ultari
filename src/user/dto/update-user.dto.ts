52d79945f61887feacd58294b2c0a65b94fb73bb
ungheung <t01047377477@gmail.com>
2024-08-29 19:21:52 +0900

    refactor : 불필요한 데코레이터 제거

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,18 +1,14 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional } from 'class-validator';
 import { UserProfileEntity } from '../entity/user-profile.entity';
 import { PartialType } from '@nestjs/mapped-types';
 import { UserEntity } from '../entity/user.entity';
 
 export class UpdateUserDto extends PartialType(UserEntity) {
-  @IsString()
-  @IsNotEmpty()
   userPassword: string;
 
-  @IsString()
   @IsOptional()
   userPhone?: string;
 
-  @IsString()
   @IsOptional()
   userEmail?: string;
 
