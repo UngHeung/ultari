36571ff9a6bef61b4c64aa0d307a7a66d55777b7
ungheung <t01047377477@gmail.com>
2024-08-31 21:10:48 +0900

    refactor : 유저 관련 요청에 guard 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserEntity } from './entity/user.entity';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -8,16 +8,19 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/')
+  @UseGuards()
   getUsers(): Promise<UserEntity[]> {
     return this.userService.getUsers();
   }
 
   @Get('/:id')
+  @UseGuards()
   getUserById(@Param('id') id: number) {
     return this.userService.getUserById(id);
   }
 
   @Patch('/:id')
+  @UseGuards()
   updateUser(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.updateUser(id, updateUserDto);
   }
