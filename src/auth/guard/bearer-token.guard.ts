4325faea48e1c781b7d6f8b5cf360b02ae6d197d
ungheung <t01047377477@gmail.com>
2024-08-31 22:30:00 +0900

    feat : bearer token guard 로직 추가

---
+++ src/auth/guard/bearer-token.guard.ts
@@ -0,0 +1,58 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class BearerTokenGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const rawToken = request.headers['authorization'];
+
+    if (!rawToken) {
+      throw new UnauthorizedException('토큰이 없습니다.');
+    }
+
+    const token = this.authService.extractToken(rawToken, true);
+    const result = await this.authService.verifyToken(token);
+    const user = await this.userService.getUserByUserAccount(result.userAccount);
+
+    request.user = user;
+    request.token = token;
+    request.tokenType = result.type;
+
+    return true;
+  }
+}
+
+@Injectable()
+export class AccessTokenGuard extends BearerTokenGuard {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await super.canActivate(context);
+    const request = context.switchToHttp().getRequest();
+
+    if (request.tokenType !== 'access') {
+      throw new UnauthorizedException('Access Token이 아닙니다.');
+    }
+
+    return true;
+  }
+}
+
+@Injectable()
+export class RefreshTokenGuard extends BearerTokenGuard {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await super.canActivate(context);
+    const request = context.switchToHttp().getRequest();
+
+    if (request.tokenType !== 'refresh') {
+      throw new UnauthorizedException('Refresh Token이 아닙니다.');
+    }
+
+    return true;
+  }
+}
