802cbef3a71a4b5ba166ec0ba6ddb7adb1ef9227
ungheung <t01047377477@gmail.com>
2024-09-01 02:54:18 +0900

    refactor : path const로 변경

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
 import { MulterModule } from '@nestjs/platform-express';
+import { PUBLIC_FOLDER_PATH } from './const/path.const';
 import { extname } from 'path';
 import { v4 as uuid } from 'uuid';
 import * as multer from 'multer';
@@ -23,7 +24,7 @@ import * as multer from 'multer';
       },
       storage: multer.diskStorage({
         destination: (request, response, callback) => {
-          callback(null, '/public/profile');
+          callback(null, PUBLIC_FOLDER_PATH);
         },
         filename: (request, file, callback) => {
           callback(null, `${uuid()}${extname(file.originalname)}`);
