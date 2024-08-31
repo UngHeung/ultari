6cdc6d69f85f8a02f9b9277ca3ba8669c7c96b35
ungheung <t01047377477@gmail.com>
2024-09-01 00:19:42 +0900

    refactor : config 값 함수로 받아오도록 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
-import { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } from './configs/const/config.const';
-import { UserEntity } from './user/entity/user.entity';
-import { UserProfileEntity } from './user/entity/user-profile.entity';
+import { getTypeOrmConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [
@@ -14,17 +12,7 @@ import { UserProfileEntity } from './user/entity/user-profile.entity';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env[DB_HOST],
-      port: +process.env[DB_PORT],
-      username: process.env[DB_USERNAME],
-      password: process.env[DB_PASSWORD],
-      database: process.env[DB_DATABASE],
-      autoLoadEntities: true,
-      entities: [UserEntity, UserProfileEntity],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(getTypeOrmConfig()),
     AuthModule,
     UserModule,
   ],
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,14 +3,16 @@ import { UserEntity } from 'src/user/entity/user.entity';
 import { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } from './const/config.const';
 import { UserProfileEntity } from 'src/user/entity/user-profile.entity';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env[DB_HOST],
-  port: +process.env[DB_PORT],
-  username: process.env[DB_USERNAME],
-  password: process.env[DB_PASSWORD],
-  database: process.env[DB_DATABASE],
-  autoLoadEntities: true,
-  entities: [UserEntity, UserProfileEntity],
-  synchronize: true,
+export const getTypeOrmConfig = (): TypeOrmModuleOptions => {
+  return {
+    type: 'postgres',
+    host: process.env[DB_HOST],
+    port: +process.env[DB_PORT],
+    username: process.env[DB_USERNAME],
+    password: process.env[DB_PASSWORD],
+    database: process.env[DB_DATABASE],
+    autoLoadEntities: true,
+    entities: [UserEntity, UserProfileEntity],
+    synchronize: true,
+  };
 };
