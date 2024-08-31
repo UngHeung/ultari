01aa6c4059edeb61d1525b3269d07fee9a2f3872
ungheung <t01047377477@gmail.com>
2024-08-31 22:21:44 +0900

    feat : basic-token guard 로직

---
+++ src/auth/guard/basic-token.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class BasicTokenGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const rawToken = request.headers['authorization'];
+
+    if (!rawToken) {
+      throw new UnauthorizedException('토큰이 없습니다.');
+    }
+
+    const token = this.authService.extractToken(rawToken, false);
+    const { userAccount, userPassword } = this.authService.decodeBasicToken(token);
+    const user = await this.authService.authenticateAccountAndPassword({ userAccount, userPassword });
+
+    request.user = user;
+
+    return true;
+  }
+}
