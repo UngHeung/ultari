295bfc99910afb87445cdeb4f77721fdf66ecd4b
ungheung <t01047377477@gmail.com>
2024-08-31 21:09:50 +0900

    style : 주석 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,35 +13,64 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
+  /**
+   * 1. receive user information
+   * 2. check already registered account, email, phone
+   * 3. save new user in user repository
+   */
   async createUser(authSignUpDto: AuthSignUpDto) {
     const { userAccount, userEmail, userPhone }: AuthSignUpDto = authSignUpDto;
     const userExists = await this.userRepository.exists({
       where: [{ userAccount }, { userEmail }, { userPhone }],
     });
 
-    console.log(userExists);
-    if (userExists) throw new ConflictException('');
+    if (userExists) {
+      throw new ConflictException();
+    }
 
     const newUser = this.userRepository.create(authSignUpDto);
+
     return await this.userRepository.save(newUser);
   }
 
+  /**
+   * 1. receive request
+   * 2. get all users from user repository
+   * 3. return users
+   */
   async getUsers(): Promise<UserEntity[]> {
     const user = await this.userRepository.find();
     return user;
   }
 
+  /**
+   * 1. receive user id
+   * 2. get user by user id from user repository
+   * 3. return user
+   */
   async getUserById(id: number): Promise<UserEntity> {
     const user = await this.userRepository.findOne({ where: { id }, relations: ['userProfile'] });
     if (!user) throw new NotFoundException(`계정이 존재하지 않습니다. ID : ${id}`);
     return user;
   }
 
+  /**
+   * 1. receive user account
+   * 2. get user by user account from user repostitory
+   * 3. return user
+   */
   async getUserByUserAccount(userAccount: string): Promise<UserEntity> {
     const user = await this.userRepository.findOne({ where: { userAccount } });
     return user;
   }
 
+  /**
+   * 1. receive data for update user information
+   * 2. request get user by user id
+   * 3. compare input password and stored password
+   * 4. update data for target user
+   * 5. save updated user
+   */
   async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
     const user = await this.getUserById(id);
     const { userPassword, userPhone, userEmail, userProfile }: UpdateUserDto = updateUserDto;
