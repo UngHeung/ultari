0360cda50aa08c2187de2792f0cdb1ee5c613568
ungheung <t01047377477@gmail.com>
2024-09-01 03:29:46 +0900

    update : 불필요 const 삭제

--- src/common/const/path.const.ts
+++ src/common/const/path.const.ts
@@ -2,10 +2,10 @@ import { join } from 'path';
 
 export const PROJECT_ROOT_PATH = process.cwd();
 export const PUBLIC_FOLDER_NAME = 'public';
-export const POSTS_FOLDER_NAME = 'posts';
+export const PROFILE_FOLDER_NAME = 'profile';
 export const TEMP_FOLDER_NAME = 'temp';
 
 export const PUBLIC_FOLDER_PATH = join(PROJECT_ROOT_PATH, PUBLIC_FOLDER_NAME);
-export const POST_IMAGE_PATH = join(PUBLIC_FOLDER_PATH, POSTS_FOLDER_NAME);
-export const POST_PUBLIC_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, POSTS_FOLDER_NAME);
+
+export const PROFILE_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, PROFILE_FOLDER_NAME);
 export const TEMP_FOLDER_PATH = join(PUBLIC_FOLDER_NAME, TEMP_FOLDER_NAME);
