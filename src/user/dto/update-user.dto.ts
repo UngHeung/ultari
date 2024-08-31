e791e3c6fc8fe7dc5719b7acf492859edcf9723c
ungheung <t01047377477@gmail.com>
2024-09-01 01:49:05 +0900

    update : userProfie -> userProfilePath로 변경

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,4 @@
 import { IsOptional } from 'class-validator';
-import { UserProfileEntity } from '../entity/user-profile.entity';
 import { PartialType } from '@nestjs/mapped-types';
 import { UserEntity } from '../entity/user.entity';
 
@@ -13,5 +12,5 @@ export class UpdateUserDto extends PartialType(UserEntity) {
   userEmail?: string;
 
   @IsOptional()
-  userProfile?: UserProfileEntity;
+  userProfilePath?: string;
 }
