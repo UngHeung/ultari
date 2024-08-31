546adae125df2937d0be70fa947cdd7748086d94
ungheung <t01047377477@gmail.com>
2024-09-01 03:11:11 +0900

    refactor : multer options 분리

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -6,32 +6,11 @@ import { PUBLIC_FOLDER_PATH } from './const/path.const';
 import { extname } from 'path';
 import { v4 as uuid } from 'uuid';
 import * as multer from 'multer';
+import { UserModule } from 'src/user/user.module';
+import { multerModuleOptions } from './multer/file-upload.multer';
 
 @Module({
-  imports: [
-    MulterModule.register({
-      limits: {
-        fileSize: 5 * 125000,
-      },
-      fileFilter: (request, file, callback) => {
-        const ext = extname(file.originalname);
-
-        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png' && ext !== '.gif') {
-          return callback(new BadRequestException('업로드 가능한 확장자 : [jpg, jpeg, png, gif]'), false);
-        }
-
-        return callback(null, true);
-      },
-      storage: multer.diskStorage({
-        destination: (request, response, callback) => {
-          callback(null, PUBLIC_FOLDER_PATH);
-        },
-        filename: (request, file, callback) => {
-          callback(null, `${uuid()}${extname(file.originalname)}`);
-        },
-      }),
-    }),
-  ],
+  imports: [MulterModule.register(multerModuleOptions)],
   exports: [CommonService],
   controllers: [CommonController],
   providers: [CommonService],
