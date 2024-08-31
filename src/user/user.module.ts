bb4377f3f09f5c5b151f23a6b1868137c593fdda
ungheung <t01047377477@gmail.com>
2024-09-01 03:30:53 +0900

    feat : serve static module 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,21 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerModuleOptions } from 'src/common/multer/file-upload.multer';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from 'src/common/const/path.const';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), forwardRef(() => AuthModule)],
+  imports: [
+    MulterModule.register(multerModuleOptions),
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
+    }),
+    TypeOrmModule.forFeature([UserEntity]),
+    forwardRef(() => AuthModule),
+  ],
   exports: [UserService],
   controllers: [UserController],
   providers: [UserService],
