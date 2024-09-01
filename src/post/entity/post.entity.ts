ae1ffc11a5587c8f08f30a69f2533500b2f2fdff
ungheung <t01047377477@gmail.com>
2024-09-01 21:29:13 +0900

    update : change properties from post entity

--- src/post/entity/post.entity.ts
+++ src/post/entity/post.entity.ts
@@ -1,5 +1,6 @@
-import { IsOptional } from 'class-validator';
+import { IsOptional, Length } from 'class-validator';
 import { BaseModel } from 'src/common/entity/base.entity';
+import { lengthValidationMessage } from 'src/common/validator/message/length-validation.message';
 import { UserEntity } from 'src/user/entity/user.entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 
@@ -11,25 +12,23 @@ export enum PublicEnum {
 
 @Entity()
 export class PostEntity extends BaseModel {
-  @Column({
-    nullable: false,
-  })
+  @Column({ nullable: false })
+  @Length(2, 20, { message: lengthValidationMessage })
   postTitle: string;
 
-  @Column({
-    nullable: false,
-  })
+  @Column({ nullable: false })
+  @Length(2, 300, { message: lengthValidationMessage })
   postContent: string;
 
   @Column()
   @IsOptional()
-  postImages: string[];
+  postImages: string; // will change type to post images entity
 
   @Column({
     enum: Object.values(PublicEnum),
     default: PublicEnum.PUBLIC,
   })
-  postPublicScope: string;
+  postVisibility: string;
 
   @ManyToOne(() => UserEntity, user => user.posts)
   PostAuthor: UserEntity;
