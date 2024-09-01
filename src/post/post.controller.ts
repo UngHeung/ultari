6f0eb272ee3878d6bb65dec90b164a741564b01a
ungheung <t01047377477@gmail.com>
2024-09-01 21:27:28 +0900

    feat : add post, get all post logic

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { PostEntity } from './entity/post.entity';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @Post()
+  createPost(@Body() dto: CreatePostDto) {
+    console.log(dto);
+  }
+
+  @Post()
+  @UseInterceptors(FilesInterceptor('images'))
+  uploadImage(@UploadedFiles() files: Express.Multer.File) {}
+
+  @Get()
+  getPosts(): Promise<PostEntity[]> {
+    return this.postService.getPosts();
+  }
 }
