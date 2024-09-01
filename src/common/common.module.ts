69921062aaa782467791c66681684c7f2e5db31f
ungheung <t01047377477@gmail.com>
2024-09-01 19:57:45 +0900

    refactor : move file rename and file remove logic from user service to common service

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,9 +3,11 @@ import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
 import { MulterModule } from '@nestjs/platform-express';
 import { multerModuleOptions } from './multer/file-upload.multer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entity/user.entity';
 
 @Module({
-  imports: [MulterModule.register(multerModuleOptions)],
+  imports: [MulterModule.register(multerModuleOptions), TypeOrmModule.forFeature([UserEntity])],
   exports: [CommonService],
   controllers: [CommonController],
   providers: [CommonService],
