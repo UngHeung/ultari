7cd7ebc19e2e9c4dcca1f2610e198d6489b6dab6
ungheung <t01047377477@gmail.com>
2024-09-03 21:21:07 +0900

    feat : add post update logic

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Controller,
   Get,
+  Param,
+  Patch,
   Post,
   Req,
   UploadedFiles,
@@ -13,6 +15,7 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { PostEntity } from './entity/post.entity';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
 export class PostController {
@@ -33,5 +36,15 @@ export class PostController {
   @UseGuards(AccessTokenGuard)
   getPosts(): Promise<PostEntity[]> {
     return this.postService.getPosts();
+  }
+
+  @Patch('/:id')
+  @UseGuards(AccessTokenGuard)
+  updatePost(
+    @Req() req,
+    @Param('id') id: number,
+    @Body() dto: UpdatePostDto,
+  ): Promise<PostEntity> {
+    return this.postService.updatePost(req.user, +id, dto);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostEntity } from './entity/post.entity';
 import { Repository } from 'typeorm';
 import { UserEntity } from 'src/user/entity/user.entity';
 import { ImageEntity } from 'src/common/entity/image.entity';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
@@ -13,6 +18,12 @@ export class PostService {
     private readonly postRepository: Repository<PostEntity>,
   ) {}
 
+  /**
+   * @param author
+   * @param CreatePostDto
+   * 1. request to create new post
+   * 2. returns created new post
+   */
   async createPost(
     author: UserEntity,
     dto: CreatePostDto,
@@ -28,14 +39,59 @@ export class PostService {
   }
 
   /**
-   * 0. for development
-   * 1. receive request
-   * 2. find all posts
-   * 3. return posts
+   * [for development]
+   * 1. find all posts
+   * 2. return posts
    */
   async getPosts(): Promise<PostEntity[]> {
-    const posts = await this.postRepository.find();
+    const posts = await this.postRepository.find({
+      relations: { author: true },
+    });
 
     return posts;
+  }
+
+  /**
+   * 1. find post by id
+   * 2. return post
+   */
+  async getPost(id: number): Promise<PostEntity> {
+    const post = await this.postRepository.findOne({
+      where: { id },
+      relations: { author: true },
+    });
+
+    return post;
+  }
+
+  /**
+   * @param id
+   * @param UpdatePostDto
+   */
+  async updatePost(
+    user: UserEntity,
+    id: number,
+    dto: UpdatePostDto,
+  ): Promise<PostEntity> {
+    const post = await this.postRepository.findOne({
+      where: {
+        author: { id: user.id },
+        id,
+      },
+    });
+
+    if (!post) {
+      throw new UnauthorizedException('본인의 게시물이 아닙니다.');
+    }
+
+    dto.title && (post.title = dto.title);
+    dto.content && (post.content = dto.content);
+    dto.images && (post.images = dto.images);
+    dto.visibility && (post.visibility = dto.visibility);
+    dto.type && (post.type = dto.type);
+
+    const newPost = this.postRepository.save(post);
+
+    return newPost;
   }
 }
