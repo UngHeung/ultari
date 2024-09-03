6f4901847abd75d280547fb4569bbe85b2f8b9a6
ungheung <t01047377477@gmail.com>
2024-09-03 18:52:06 +0900

    update : add images property

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,9 +1,15 @@
 import { PickType } from '@nestjs/mapped-types';
 import { PostEntity } from '../entity/post.entity';
+import { IsOptional, IsString } from 'class-validator';
+import { ImageEntity } from 'src/common/entity/image.entity';
 
 export class CreatePostDto extends PickType(PostEntity, [
   'title',
   'content',
   'visibility',
   'type',
-]) {}
+]) {
+  @IsString({ each: true })
+  @IsOptional()
+  images?: ImageEntity[] = [];
+}
