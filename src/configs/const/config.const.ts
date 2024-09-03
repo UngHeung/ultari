0f7ada7b892ed5e544f4c771d4f7c9b3d148dda4
ungheung <t01047377477@gmail.com>
2024-09-03 22:07:30 +0900

    feat : add jwt const

--- src/configs/const/config.const.ts
+++ src/configs/const/config.const.ts
@@ -1,3 +1,7 @@
+export const JWT_SECRET = 'JWT_SECRET';
+export const JWT_ACCESS_EXPIRES_IN = 'JWT_ACCESS_EXPIRES_IN';
+export const JWT_REFRESH_EXPIRES_IN = 'JWT_REFRESH_EXPIRES_IN';
+
 export const DB_HOST = 'DB_HOST';
 export const DB_PORT = 'DB_PORT';
 export const DB_USERNAME = 'DB_USERNAME';
