51cd7f942cd70a504a0476ae32f49d3c8985fd51
ungheung <t01047377477@gmail.com>
2024-09-04 23:32:08 +0900

    feat : add post image path

--- src/common/const/path.const.ts
+++ src/common/const/path.const.ts
@@ -3,9 +3,11 @@ import { join } from 'path';
 export const PROJECT_ROOT_PATH = process.cwd();
 export const PUBLIC_FOLDER_NAME = 'public';
 export const PROFILE_FOLDER_NAME = 'profile';
+export const POST_FOLDER_NAME = 'post';
 export const TEMP_FOLDER_NAME = 'temp';
 
 export const PUBLIC_FOLDER_PATH = join(PROJECT_ROOT_PATH, PUBLIC_FOLDER_NAME);
 
 export const PROFILE_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, PROFILE_FOLDER_NAME);
+export const POST_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, POST_FOLDER_NAME);
 export const TEMP_FOLDER_PATH = join(PUBLIC_FOLDER_NAME, TEMP_FOLDER_NAME);
