286ea2f31b904150476a466e299e7e95f35b039e
ungheung <t01047377477@gmail.com>
2024-09-04 23:31:43 +0900

    feat : create image single upload, multiple upload logic

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CommonService } from './common.service';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 
 @Controller('common')
 export class CommonController {
   constructor(private readonly commonService: CommonService) {}
+
+  @Post('/image')
+  @UseInterceptors(FileInterceptor('image'))
+  @UseGuards(AccessTokenGuard)
+  postImage(@UploadedFile() file?: Express.Multer.File) {
+    return { fileName: file.filename };
+  }
+
+  @Post('/images')
+  @UseInterceptors(FilesInterceptor('images'))
+  @UseGuards(AccessTokenGuard)
+  PostImages(@UploadedFiles() files?: Express.Multer.File[]) {
+    const results = {
+      fileNames: [],
+    };
+
+    for (let i = 0; i < files.length; i++) {
+      results.fileNames.push(files[i].filename);
+    }
+
+    return results;
+  }
 }
