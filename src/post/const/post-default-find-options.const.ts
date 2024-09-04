faea78c975c772d31fdb059dbe7198bda937ac94
ungheung <t01047377477@gmail.com>
2024-09-04 15:14:02 +0900

    feat : add default find options

---
+++ src/post/const/post-default-find-options.const.ts
@@ -0,0 +1,9 @@
+import { FindManyOptions } from 'typeorm';
+import { PostEntity } from '../entity/post.entity';
+
+export const POST_DEFAULT_FIND_OPTIONS: FindManyOptions<PostEntity> = {
+  relations: {
+    author: true,
+    images: true,
+  },
+};
