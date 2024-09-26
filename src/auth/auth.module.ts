b555b23d9add4f542048c346c355a38c396022c2
ungheung <t01047377477@gmail.com>
2024-09-26 15:11:33 +0900

    update: module 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/entity/user.entity';
+import { TeamModule } from 'src/team/team.module';
 
 @Module({
   imports: [
     JwtModule.register({}),
     TypeOrmModule.forFeature([UserEntity]),
     UserModule,
+    TeamModule,
   ],
   exports: [AuthService],
   controllers: [AuthController],
--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/entity/user.entity';
 import { TeamEntity } from './entity/team.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TeamEntity, UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([TeamEntity, UserEntity]),
+    forwardRef(() => AuthModule),
+    forwardRef(() => UserModule),
+  ],
   controllers: [TeamController],
   providers: [TeamService],
 })
