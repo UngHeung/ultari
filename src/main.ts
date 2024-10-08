f4c3f41e3df2adee8e0dbc2a19c1407805ab314c
ungheung <t01047377477@gmail.com>
2024-10-09 02:29:16 +0900

    update : 개발단계에서  fe서버에 public 폴더 사용 가능하도록 변경

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -14,6 +15,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.useStaticAssets('public', { prefix: '/public/' });
   app.enableCors({
     origin: [
       'http://localhost:3000',
