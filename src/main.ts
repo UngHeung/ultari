aba942e55a41e211bf603b131e2350706de76574
ungheung <t01047377477@gmail.com>
2024-10-10 15:31:24 +0900

    test : cors test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -18,7 +20,12 @@ async function bootstrap() {
 
   app.useStaticAssets('public', { prefix: '/public/' });
   app.enableCors({
-    origin: true,
+    origin: [
+      'https://ultari.xyz',
+      'https://www.ultari.xyz',
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
     credentials: true,
   });
   await app.listen(3000);
