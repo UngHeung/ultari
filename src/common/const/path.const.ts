c1b8ffc083a4a54405f74ef5c7d3c1770ac8a2c6
ungheung <t01047377477@gmail.com>
2024-10-13 19:50:01 +0900

    update : s3 path 추가

--- src/common/const/path.const.ts
+++ src/common/const/path.const.ts
@@ -1,13 +1,26 @@
 import { join } from 'path';
 
 export const PROJECT_ROOT_PATH = process.cwd();
+export const PROJECT_S3_PATH = `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_S3_BUCKET_NAME}`;
+
 export const PUBLIC_FOLDER_NAME = 'public';
 export const PROFILE_FOLDER_NAME = 'profile';
 export const POST_FOLDER_NAME = 'post';
 export const TEMP_FOLDER_NAME = 'temp';
 
-export const PUBLIC_FOLDER_PATH = join(PROJECT_ROOT_PATH, PUBLIC_FOLDER_NAME);
+export const PUBLIC_ROOT_FOLDER_PATH = join(
+  PROJECT_ROOT_PATH,
+  PUBLIC_FOLDER_NAME,
+);
+export const PUBLIC_AWS_FOLDER_PATH = join(PROJECT_S3_PATH, PUBLIC_FOLDER_NAME);
 
 export const PROFILE_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, PROFILE_FOLDER_NAME);
 export const POST_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, POST_FOLDER_NAME);
 export const TEMP_FOLDER_PATH = join(PUBLIC_FOLDER_NAME, TEMP_FOLDER_NAME);
+
+export const PROFILE_IMAGE_S3_PATH = join(
+  PUBLIC_FOLDER_NAME,
+  PROFILE_FOLDER_NAME,
+);
+export const POST_IMAGE_S3_PATH = join(PUBLIC_FOLDER_NAME, POST_FOLDER_NAME);
+export const TEMP_FOLDER_S3_PATH = join(PUBLIC_FOLDER_NAME, TEMP_FOLDER_NAME);
