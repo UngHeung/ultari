1f04d52f825a6bb64226ffe5e90406c3585d85c8
ungheung <t01047377477@gmail.com>
2024-10-14 23:57:12 +0900

    feat : 유저 프로필 s3 버켓에 저장 및 프로필 객체 생성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,17 +4,19 @@ import {
   Get,
   Param,
   Patch,
+  Post,
   Req,
   UploadedFile,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserEntity } from './entity/user.entity';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 import { ImageTypeEnum } from 'src/common/enum/image.enum';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entity/user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
@@ -55,17 +57,29 @@ export class UserController {
     return this.userService.getUserById(id);
   }
 
+  @Post('/profile')
+  @UseGuards(AccessTokenGuard)
+  @UseInterceptors(FileInterceptor('profile', { storage: memoryStorage() }))
+  async uploadProfile(@UploadedFile() file: Express.Multer.File) {
+    const { fileName } = await this.userService.saveImage(file);
+
+    console.log('fileName : ', fileName);
+
+    return { fileName };
+  }
+
   @Patch('/')
   @UseGuards(AccessTokenGuard)
-  @UseInterceptors(FileInterceptor('userProfile'))
   async updateUser(@Req() req, @Body() dto: UpdateUserDto) {
     const user = await this.userService.updateUser(req.user, dto);
 
-    await this.userService.createProfileImage({
-      user,
-      path: dto.path,
-      type: ImageTypeEnum.PROFILE_IMAGE,
-    });
+    if (dto.path) {
+      await this.userService.createProfileImage({
+        user,
+        path: dto.path,
+        type: ImageTypeEnum.PROFILE_IMAGE,
+      });
+    }
 
     return await this.userService.getUserById(req.user.id);
   }
