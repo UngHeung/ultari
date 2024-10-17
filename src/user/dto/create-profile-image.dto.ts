cf0b8fddb7a90fe3e588da401fb7865e3f2d50ba
ungheung <t01047377477@gmail.com>
2024-10-18 01:20:49 +0900

    del : 미사용 dto 삭제

--- src/user/dto/create-profile-image.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PickType } from '@nestjs/mapped-types';
-import { ProfileImageEntity } from '../entity/profile-image.entity';
-
-export class CreateProfileImageDto extends PickType(ProfileImageEntity, [
-  'user',
-  'path',
-  'type',
-]) {}
