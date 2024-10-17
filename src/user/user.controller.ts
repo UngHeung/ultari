4761deeb5ef12cbdf19f1fce417044f3a9cd299f
ungheung <t01047377477@gmail.com>
2024-10-18 01:23:07 +0900

    update : 유저 서비스 내 변경된 함수명 적용

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,8 +13,7 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
-import { ImageTypeEnum } from 'src/common/enum/image.enum';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDataDto } from './dto/update-user-data.dto';
 import { UserEntity } from './entity/user.entity';
 import { UserService } from './user.service';
 
@@ -24,37 +23,32 @@ export class UserController {
 
   @Get('/')
   @UseGuards(AccessTokenGuard)
-  getUsers(): Promise<UserEntity[]> {
-    return this.userService.getUsers();
+  GetUserAll(): Promise<UserEntity[]> {
+    return this.userService.getUsersAll();
   }
 
   @Get('/myinfo')
   @UseGuards(AccessTokenGuard)
-  getMyInfo(@Req() req) {
-    return this.userService.getUserById(req.user.id);
+  getMyUserData(@Req() req) {
+    return this.userService.getUserData(req.user.id);
   }
 
   @Get('/myinfo/team')
   @UseGuards(AccessTokenGuard)
   getMyInfoAndTeam(@Req() req) {
-    return this.userService.getUserWithTeam(req.user.id);
+    return this.userService.getUserDataAndTeam(req.user.id);
   }
 
   @Get('/myinfo/post')
   @UseGuards(AccessTokenGuard)
-  getMyINfoAndPost(@Req() req) {
-    return this.userService.getUserWithPosts(req.user.id);
-  }
-
-  @Get('/myinfo/team-and-post')
-  getUserWithTeamAndPosts(@Req() req) {
-    return this.userService.getUserWithTeamAndPosts(req.user.id);
+  getMyInfoAndPost(@Req() req) {
+    return this.userService.getUserDataAndPosts(req.user.id);
   }
 
   @Get('/:id')
   @UseGuards(AccessTokenGuard)
   getUserById(@Param('id') id: number) {
-    return this.userService.getUserById(id);
+    return this.userService.getUserData(id);
   }
 
   @Post('/profile')
@@ -63,24 +57,14 @@ export class UserController {
   async uploadProfile(@UploadedFile() file: Express.Multer.File) {
     const { fileName } = await this.userService.saveImage(file);
 
-    console.log('fileName : ', fileName);
-
     return { fileName };
   }
 
   @Patch('/')
   @UseGuards(AccessTokenGuard)
-  async updateUser(@Req() req, @Body() dto: UpdateUserDto) {
-    const user = await this.userService.updateUser(req.user, dto);
+  async updateUser(@Req() req, @Body() dto: UpdateUserDataDto) {
+    const user = await this.userService.getUserData(req.user.id);
 
-    if (dto.path) {
-      await this.userService.createProfileImage({
-        user,
-        path: dto.path,
-        type: ImageTypeEnum.PROFILE_IMAGE,
-      });
-    }
-
-    return await this.userService.getUserById(req.user.id);
+    return this.userService.updateUserData(req.user, dto);
   }
 }
