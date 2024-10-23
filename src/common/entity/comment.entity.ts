939007d4acc2ff5753592271fa020f59a6534733
ungheung <t01047377477@gmail.com>
2024-10-23 12:23:48 +0900

    feat : base comment model 생성

--- src/common/entity/comment.entity.ts
+++ src/common/entity/comment.entity.ts
@@ -1,26 +1,9 @@
 import { IsString } from 'class-validator';
 import { BaseModel } from 'src/common/entity/base.entity';
-import { PostEntity } from 'src/post/entity/post.entity';
-import { UserEntity } from 'src/user/entity/user.entity';
-import { Column, ManyToOne } from 'typeorm';
+import { Column } from 'typeorm';
 
-interface HasCommentEntityOptions {
-  comments: CommentBase<HasCommentsEntities>[];
-}
-
-export type HasCommentsEntities = PostEntity;
-
-export class CommentBase<T extends HasCommentEntityOptions> extends BaseModel {
+export class CommentModel extends BaseModel {
   @Column({ nullable: false })
   @IsString()
   content: string;
-
-  @ManyToOne(() => UserEntity, user => user.comments)
-  writer: UserEntity;
-
-  @ManyToOne(
-    () => Object as () => T,
-    (target: T) => (target as HasCommentEntityOptions).comments,
-  )
-  target: T;
 }
