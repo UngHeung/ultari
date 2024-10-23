8c7087d750bdd9ccd5ac1832ff18c947e6e60284
ungheung <t01047377477@gmail.com>
2024-10-23 12:30:53 +0900

    style : 미사용 import 제거

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -3,7 +3,6 @@ import { IsOptional, IsString } from 'class-validator';
 import { PostCommentEntity } from '../entity/post-comment.entity';
 import { PostEntity } from '../entity/post.entity';
 import { ContentTypeEnum, PublicEnum } from '../enum/post.enum';
-import { CommentEntity } from 'src/comment/entity/comment.entity';
 
 export class CreatePostDto extends PickType(PostEntity, [
   'title',
